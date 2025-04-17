61be617cbd1b0bfc11dd27233c387da21d39a52c
Dee Garrin <mgarrin50@gmail.com>
2025-04-17 16:10:04 -0400

    w2w enemy movement
    waypoint to waypoint movement implemented on a basic level

---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] waypoints;
+    [SerializeField]
+    private float moveSpeed = 2f;
+    private int waypointIndex = 0;
+    [SerializeField]
+    private bool isLoop = true;
+
+    private void Start()
+    {
+ 
+    }
+    private void Update()
+    {
+        Vector3 destination = waypoints[waypointIndex].transform.position;
+        Vector3 newPos = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+        transform.position = newPos;
+
+        float distance = Vector3.Distance(transform.position, destination);
+
+        if (distance <= 0.1)
+        {
+            if (waypointIndex < waypoints.Length - 1)
+            {
+                waypointIndex++;
+                print("Going to waypoint " + waypointIndex);
+            }
+            else
+            {
+                if (isLoop)
+                {
+                    print("Looping...");
+                    waypointIndex = 0;
+                }
+            }
+        }
+        else
+        {
+            print(distance);
+        }
+    }
+}
