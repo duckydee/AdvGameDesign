e6864d5a7bfd303502f1a5487d1b0fa139be0c63
Matthew Cole <mattcole04@gmail.com>
2025-04-20 18:23:55 -0400

    Hotbar
    added hot bar that pulls information directly from the top row of the inventory.

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,37 +1,116 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class InventoryManager : MonoBehaviour
 {
-    // Inventory/item features created with the help of ErenCode tutorials
+    // item features created with the help of ErenCode tutorials
+    [SerializeField] private GameObject itemCursor;
+    // Inventory
     [SerializeField] private GameObject slotHolder;
+    [SerializeField] private GameObject HotbarSlotHolder;
     [SerializeField] private ItemClass itemToAdd;
     [SerializeField] private ItemClass itemToRemove;
 
-    public List<SlotClass> items = new List<SlotClass>();
+    [SerializeField] private SlotClass[] startingItems;
+
+    [SerializeField] private GameObject hotbarSelector;
+    [SerializeField] private int selectedSlotIndex = 0;
+    public ItemClass selectedItem;
+
+    private SlotClass[] items;
+    private SlotClass[] hotbarItems;
 
     // This happens as soon as the game starts, meaning that it's just a test feature and will need to be removed in the future
-    private GameObject[] slots; 
-    public void Start()
+    private GameObject[] slots;
+    private GameObject[] hotbarSlots;
+
+    private SlotClass movingSlot;
+    private SlotClass tempSlot;
+    private SlotClass originalSlot;
+    bool isMovingItem;
+
+    private void Start()
     {
         slots = new GameObject[slotHolder.transform.childCount];
+        items = new SlotClass[slots.Length];
+
+        hotbarSlots = new GameObject[HotbarSlotHolder.transform.childCount];
+
+        for (int i = 0; i < hotbarSlots.Length; i++)
+            hotbarSlots[i] = HotbarSlotHolder.transform.GetChild(i).gameObject;
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i] = new SlotClass();
+        }
+        for (int i = 0; i < startingItems.Length; i++)
+        {
+            items[i] = startingItems[i];
+        }
         //set all the slots
-        for(int i = 0; i < slotHolder.transform.childCount; i++)
+        for (int i = 0; i < slotHolder.transform.childCount; i++)
             slots[i] = slotHolder.transform.GetChild(i).gameObject;
 
         RefreshUI();
 
-        Add(itemToAdd);
+        Add(itemToAdd, 1);
         Remove(itemToRemove);
     }
+    private void Update()
+    {
+        itemCursor.SetActive(isMovingItem);
+        itemCursor.transform.position = Input.mousePosition;
+        if (isMovingItem)
+            itemCursor.GetComponent<Image>().sprite = movingSlot.GetItem().itemIcon;
+             
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            // find the Closest slot to cursor
+            if (isMovingItem)
+            {
+                EndItemMove();
+            }
+            else
+            {
+                BeginItemMove();
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            // find the Closest slot to cursor
+            if (isMovingItem)
+            {
+                EndItemMove_Single();
+            }
+            else
+            {
+                BeginItemMove_Half();
+            }
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) // scrolling up 
+        {
+            selectedSlotIndex = Mathf.Clamp(selectedSlotIndex + 1, 0, hotbarSlots.Length - 1);
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0) // scrolling down
+        {
+            selectedSlotIndex = Mathf.Clamp(selectedSlotIndex - 1, 0, hotbarSlots.Length - 1);
+        }
+
+        hotbarSelector.transform.position = hotbarSlots[selectedSlotIndex].transform.position;
+        selectedItem = items[selectedSlotIndex].GetItem();
+    }
+
+    #region Inventory Utils
     public void RefreshUI()
     {
         for (int i = 0; i < slots.Length; i++)
         {
-            try 
+            try
             {
                 slots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
                 slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].GetItem().itemIcon;
@@ -44,28 +123,59 @@ public class InventoryManager : MonoBehaviour
                     slots[i].transform.GetChild(1).GetComponent<Text>().text = "";
                 }
             }
-            catch 
+            catch
             {
                 slots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
                 slots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
                 slots[i].transform.GetChild(1).GetComponent<Text>().text = "";
-            }  
+            }
+        }
+        RefreshHotbar();
+    }
+    public void RefreshHotbar()
+    {
+        for (int i = 0; i < hotbarSlots.Length; i++)
+        {
+            try
+            {
+                hotbarSlots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
+                hotbarSlots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].GetItem().itemIcon;
+                if (items[i].GetItem().isStackable)
+                {
+                    hotbarSlots[i].transform.GetChild(1).GetComponent<Text>().text = items[i].GetQuantity() + "";
+                }
+                else
+                {
+                    hotbarSlots[i].transform.GetChild(1).GetComponent<Text>().text = "";
+                }
+            }
+            catch
+            {
+                hotbarSlots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
+                hotbarSlots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
+                hotbarSlots[i].transform.GetChild(1).GetComponent<Text>().text = "";
+            }
         }
     }
 
-    public bool Add(ItemClass item)
+    public bool Add(ItemClass item, int quantity)
     {
         //  items.Add(item);
         // Check if inventory contains item
         SlotClass slot = Contains(item);
         if (slot != null && slot.GetItem().isStackable)
-            slot.AddQuantity(1);
+            slot.AddQuantity(quantity);
         else
         {
-            if (items.Count < slots.Length)
-                items.Add(new SlotClass(item, 1));
-            else
-                return false;
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i].GetItem() == null)
+                {
+                    items[i].AddItem(item, quantity);
+                    break;
+                }
+            }
+
         }
 
         RefreshUI();
@@ -76,22 +186,25 @@ public class InventoryManager : MonoBehaviour
     {
         //items.Remove(item);
         SlotClass temp = Contains(item);
-        if ( (temp != null))
+        if (temp != null)
         {
             if (temp.GetQuantity() > 1)
-            temp.SubQuantity(1);
+            {
+                temp.SubQuantity(1);
+            }
             else
             {
-                SlotClass slotToRemove = new SlotClass();
-                foreach (SlotClass slot in items)
+                int slotToRemoveIndex = 0;
+
+                for (int i = 0; i < items.Length; i++)
                 {
-                    if (slot.GetItem() == item)
+                    if (items[i].GetItem() == item)
                     {
-                        slotToRemove = slot;
+                        slotToRemoveIndex = i;
                         break;
                     }
                 }
-                items.Remove(slotToRemove);
+                items[slotToRemoveIndex].Clear();
             }
         }
         else
@@ -105,12 +218,135 @@ public class InventoryManager : MonoBehaviour
 
     public SlotClass Contains(ItemClass item)
     {
-        foreach (SlotClass slot in items)
+        for (int i = 0; i < items.Length; i++)
         {
-            if (slot.GetItem() == item)
-                return slot;
+            if (items[i].GetItem() == item)
+                return items[i];
         }
-
         return null;
     }
+    #endregion Inventory Utils
+
+    #region Moving Stuff
+    private bool BeginItemMove()
+    {
+        originalSlot = GetClosestSlot();
+        if (originalSlot == null || originalSlot.GetItem() == null)
+        {
+            return false; // There is no item to move!
+        }
+        movingSlot = new SlotClass(originalSlot);
+        originalSlot.Clear();
+        isMovingItem = true;
+        RefreshUI();
+        return true;
+    }
+
+    private bool BeginItemMove_Half()
+    {
+        originalSlot = GetClosestSlot();
+        if (originalSlot == null || originalSlot.GetItem() == null)
+        {
+            return false; // There is no item to move!
+        }
+        movingSlot = new SlotClass(originalSlot.GetItem(),Mathf.CeilToInt( originalSlot.GetQuantity() / 2f));
+        originalSlot.SubQuantity(Mathf.CeilToInt(originalSlot.GetQuantity() / 2f));
+        isMovingItem = true;
+        RefreshUI();
+        return true;
+    }
+
+    private bool EndItemMove()
+    {
+        originalSlot = GetClosestSlot();
+        if (originalSlot == null)
+        {
+            Add(movingSlot.GetItem(), movingSlot.GetQuantity());
+            movingSlot.Clear();
+        }
+        else
+        {
+            if (originalSlot.GetItem() != null)
+            {
+                if (originalSlot.GetItem() == movingSlot.GetItem())
+                {
+                    if (originalSlot.GetItem().isStackable)
+                    {
+                        originalSlot.AddQuantity(movingSlot.GetQuantity());
+                        movingSlot.Clear();
+                    }
+                    else
+                        return false;
+                }
+                else
+                {
+                    tempSlot = new SlotClass(originalSlot); // a = b
+                    originalSlot.AddItem(movingSlot.GetItem(), movingSlot.GetQuantity()); // b = c
+                    movingSlot.AddItem(tempSlot.GetItem(), tempSlot.GetQuantity());// a = c
+
+                    RefreshUI();
+                    return true;
+                }
+            }
+            else //place item as usual
+            {
+                originalSlot.AddItem(movingSlot.GetItem(), movingSlot.GetQuantity());
+                movingSlot.Clear();
+            }
+        }
+        RefreshUI();
+        isMovingItem = false;
+        return true;
+    }
+
+    private bool EndItemMove_Single()
+    {
+        originalSlot = GetClosestSlot();
+        if (originalSlot == null)
+        {
+            return false; // There is no item to move!
+        }
+        if (originalSlot.GetItem() != null && originalSlot.GetItem() != movingSlot.GetItem())
+        {
+
+            return false;
+
+        }
+
+        movingSlot.SubQuantity(1);
+
+        if (originalSlot.GetItem() != null && originalSlot.GetItem() == movingSlot.GetItem())
+        {
+            originalSlot.AddQuantity(1);
+        }
+        else
+        {
+            originalSlot.AddItem(movingSlot.GetItem(), 1);
+        }
+
+        if (movingSlot.GetQuantity() < 1)
+        {
+            isMovingItem = false;
+            movingSlot.Clear();
+        }
+        else
+            isMovingItem = true;
+
+        RefreshUI();
+        return true;
+    }
+    private SlotClass GetClosestSlot()
+    {
+        Debug.Log(Input.mousePosition);
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (Vector2.Distance(slots[i].transform.position, Input.mousePosition) <= 32)
+            {
+                return items[i];
+            }
+        }
+        return null; 
+    }
+    #endregion Moving Stuff
 }
--- Assets/Scripts/SlotClass.cs
+++ Assets/Scripts/SlotClass.cs
@@ -12,16 +12,33 @@ public class SlotClass
         quantity = 0;
     }
 
-        public SlotClass(ItemClass _item, int _quantity)
-        {
+    public SlotClass(SlotClass slot)
+    {
+        item = slot.item;
+        quantity = slot.quantity;
+    }
+
+    public SlotClass(ItemClass _item, int _quantity)
+    {
         item = _item;
         quantity = _quantity;
-        }
+    }
+
+    public void Clear()
+    {
+        this.item = null;
+        this.quantity = 0;   
+    }
 
     public ItemClass GetItem() { return item; }
     public int GetQuantity() { return quantity; }
     public void AddQuantity(int _quantity) { quantity += _quantity; }
     public void SubQuantity(int _quantity) { quantity -= _quantity; }
+    public void AddItem(ItemClass item, int quantity) 
+    {
+        this.item = item;
+        this.quantity = quantity;
+    }
 
 
 }
