52e59d706cd76de16967701abf860e5371f62c2a
Matthew Cole <mattcole04@gmail.com>
2025-05-08 20:16:49 -0400

    Items added
    all items now do things
    Pick Up: E
    Use: Space
    Drop: Q

--- Assets/Scripts/GateBehaviour.cs
+++ Assets/Scripts/GateBehaviour.cs
@@ -1,15 +1,11 @@
+using System;
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class GateBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private GameObject generator;
+    private void Update()
     {
         
     }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,12 +7,21 @@ public class Health : MonoBehaviour
 
     public AudioSource hurtSound;
 
+    public void TakeHealing(float heal)
+    {
+        playerHealth += heal;
+        Debug.Log("Player health: " + playerHealth);
+        if(playerHealth > 100)
+        {
+            playerHealth = 100;
+        }
+    }
     public void TakeDamage(float damage)
     {
         playerHealth-= damage;
         hurtSound.Play();
         Debug.Log("Player health: "+ playerHealth);
-        if (playerHealth >= 0){
+        if (playerHealth <= 0){
             //Switch to game over screen
             SceneManager.LoadScene("GameOver");
         }
